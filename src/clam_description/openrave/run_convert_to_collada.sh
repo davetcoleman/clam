27c75314b7e1d56a1da67bdb25f960cd9d697e36
Dave Coleman <davetcoleman@gmail.com>
2013-01-10 10:48:13 -0700

    Updated Moveit Config and Gazebo files

--- src/clam_description/openrave/run_convert_to_collada.sh
+++ src/clam_description/openrave/run_convert_to_collada.sh
@@ -1,12 +1,14 @@
 # Convert URDF to collada format
-rosrun collada_urdf urdf_to_collada ../urdf/clam.urdf clam.dae
+#rosrun collada_urdf urdf_to_collada ../urdf/clam.urdf clam.dae
+rosrun collada_urdf urdf_to_collada clam.urdf clam.dae
 
 # Install openrave here: http://openrave.org/docs/latest_stable/install/#install
 
 # View list of links in model:
-/usr/bin/openrave-robot.py clam.dae --info links
+#/usr/bin/openrave-robot.py clam.dae --info links
+#/usr/bin/openrave-robot.py clam.dae --info links
 
 # Launch openrave
-read -p "Press any key to launch openrave";
-openrave clam.dae
+#read -p "Press any key to launch openrave";
+#openrave clam.dae
 
