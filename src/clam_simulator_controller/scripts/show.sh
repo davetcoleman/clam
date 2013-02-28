a3cae4162b0f81edc7dc08a899fdf33385568d19
Dave Coleman <davetcoleman@gmail.com>
2013-02-27 17:43:50 -0700

    Consolidated clam_simulator_controller pkg

--- src/clam_simulator_controller/scripts/show.sh
+++
@@ -1,9 +0,0 @@
-python pose_zero.py
-read -p "Press key"
-python pose_cobra.py
-read -p "Press key"
-python pose_cobra.py
-read -p "Press key"
-python pose_cobra.py
-read -p "Press key"
-python pose_sleep.py
