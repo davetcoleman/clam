5f0bd4b537e6aa6e2c74d2c7358a3e7ea0b3bc05
Dave Coleman <davetcoleman@gmail.com>
2013-04-18 16:54:32 -0600

    Cleaning up repo

--- clam_ik/scripts/run_generate_ikfast.sh
+++ clam_ik/scripts/run_generate_ikfast.sh
@@ -9,4 +9,7 @@ read -p "Press any key to see link info";
 read -p "Press any key to continue";
 # Now create IK. Make sure urdf/dae file location path is correct
 #python /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/ikfast.py --robot=clam.dae --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61.cpp #--freeindex=2
-python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=clam.robot.xml --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61.cpp #--freeindex=2
+#python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=clam.robot.xml --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61.cpp #--freeindex=2
+
+#python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=clam.robot.xml --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61.cpp --freeindex=3
+python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=clam.robot.xml --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61.cpp --freeindex=5
