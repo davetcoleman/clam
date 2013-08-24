9bc487bc17bef3375da1944b2ca7eb45971234ed
Dave Coleman <davetcoleman@gmail.com>
2013-08-23 21:03:43 -0700

    Cleaned up old Gazebo code

--- create_plugin_ikfast.sh
+++
@@ -1,3 +0,0 @@
-# be careful - this will overwrite changes
-#rosrun moveit_ikfast_converter create_ikfast_moveit_plugin.py clam arm clam_moveit_ikfast_plugin ~/ros/clam/src/clam_ik/clam_openrave/output_ikfast61.cpp 
-rosrun moveit_ikfast create_ikfast_moveit_plugin.py clam arm clam_moveit_ikfast_plugin clam_moveit_ikfast_plugin/src/clam_arm_ikfast_solver.cpp
